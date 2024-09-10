b244950ad0149fcc125a3465162481d8b1a5fa92
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2024-09-10 14:50:23 -0500

    Added .sh files

---
+++ process.sh
@@ -0,0 +1,3 @@
+./splitall.sh
+python3 automfcc.py
+python3 classify.py
---
+++ splitall.sh
@@ -0,0 +1,5 @@
+./split.sh audio/21525/21525-01.mp3
+./split.sh audio/23723/23723-01.mp3
+./split.sh audio/19839/19839-01.mp3
+
+python3 clean.py
