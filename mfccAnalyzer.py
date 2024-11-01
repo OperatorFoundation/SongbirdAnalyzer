e1a95a7ff2933c5b1f12b3bed06ce329e310d87f
consuelita <adelita@operatorfoundation.org>
2024-11-01 18:00:43 -0500

    Extract MFCC without creating charts

--- mfccAnalyzer.py
+++ mfccAnalyzer.py
@@ -1,4 +1,5 @@
 import os
+from random import sample
 
 import librosa.display
 import matplotlib.pyplot as plt
@@ -7,7 +8,7 @@ import numpy as np
 from constants import chartDirectory, imageFileExtension
 
 # create and save MFCC charts for all the audio files in a given directory
-def createMfccsFrom(directory: str):
+def create_mfcc_charts_from(directory: str):
     for filename in os.listdir(directory):
         # Create file path
         filePath = os.path.join(directory, filename)
@@ -16,34 +17,37 @@ def createMfccsFrom(directory: str):
         if os.path.isfile(filePath) and filename.lower().endswith('.wav'):
             # Remove the extension from the filename
             chartName = os.path.splitext(filename)[0]
-            createMfccFrom(filePath, chartName)
+            create_mfcc_chart_from(filePath, chartName)
 
-def createMfccFrom(audioFile: str, chartName: str):
+def extract_mfcc_from(audioFile: str):
+    audio, sample_rate = librosa.load(audioFile)
+    mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=13)
+    return mfcc, sample_rate
+
+
+def create_mfcc_chart_from(audioFile: str, chartName: str):
     chartType = "MFCC" # This is only used for filenames
     chartName = chartName + chartType
 
-    # load audio files with librosa
-    signal, sr = librosa.load(audioFile)
-
     ## Extracting MFCCs
-    mfccs = librosa.feature.mfcc(y=signal, n_mfcc=13, sr=sr)
+    mfcc, sample_rate = extract_mfcc_from(audioFile)
 
     ## Visualising MFCCs
     plt.figure(figsize=(25, 10))
-    librosa.display.specshow(mfccs,
+    librosa.display.specshow(mfcc,
                              x_axis="time",
-                             sr=sr)
+                             sr=sample_rate)
     plt.colorbar(format="%+2.f")
 
     ## Computing first / second MFCCs derivatives
 
     # TODO: Try calculating without the deltas for a smaller dataset
-    delta_mfccs = librosa.feature.delta(mfccs)
-    delta2_mfccs = librosa.feature.delta(mfccs, order=2)
+    delta_mfcc = librosa.feature.delta(mfcc)
+    delta2_mfcc = librosa.feature.delta(mfcc, order=2)
     plt.figure(figsize=(25, 10))
-    librosa.display.specshow(delta_mfccs,
+    librosa.display.specshow(delta_mfcc,
                              x_axis="time",
-                             sr=sr)
+                             sr=sample_rate)
     plt.colorbar(format="%+2.f")
 
     figure = plt.figure(figsize=(25, 10))
@@ -51,12 +55,12 @@ def createMfccFrom(audioFile: str, chartName: str):
     # Adds a title to the top of the chart
     figure.suptitle(chartName)
 
-    librosa.display.specshow(delta2_mfccs,
+    librosa.display.specshow(delta2_mfcc,
                              x_axis="time",
-                             sr=sr)
+                             sr=sample_rate)
     plt.colorbar(format="%+2.f")
 
-    mfccs_features = np.concatenate((mfccs, delta_mfccs, delta2_mfccs))
+    mfcc_features = np.concatenate((mfcc, delta_mfcc, delta2_mfcc))
 
     # # Save the DataFrame to an Excel file
     # tableFilePath = tablesDirectory + filename + ".xlsx"
@@ -67,3 +71,4 @@ def createMfccFrom(audioFile: str, chartName: str):
 
     #  Save the chart as a png (this must be done before calling .show() or only a blank image will be saved)
     plt.savefig(chartDirectory + chartName + imageFileExtension)
+    
