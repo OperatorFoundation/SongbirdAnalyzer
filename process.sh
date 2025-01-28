39dc4db17f4e5a10805b2cf3cb2bc3f45a6255c2
consuelita <adelita@operatorfoundation.org>
2025-01-28 12:41:42 -0600

    Update process.sh

--- process.sh
+++ process.sh
@@ -2,7 +2,7 @@ USERS_DIR=os.listdir("audio/Tests")
 
 ./splitall.sh
 
-python3 automfcc.py
-python3 classify.py
-python3 merge.py
+python3 automfcc.py $USERS_DIR
+python3 classify.py $USERS_DIR
+python3 merge.py $USERS_DIR
 
