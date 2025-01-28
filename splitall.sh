ee2bec5662a7a83f2b0207110edef96284fdcbba
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2025-01-28 12:45:51 -0600

    Use user directory argument

--- splitall.sh
+++ splitall.sh
@@ -1,5 +1,5 @@
-./split.sh audio/Tests/21525/21525-01.mp3
-./split.sh audio/Tests/23723/23723-01.mp3
-./split.sh audio/Tests/19839/19839-01.mp3
+./split.sh $1/21525/21525-01.mp3
+./split.sh $1/audio/Tests/23723/23723-01.mp3
+./split.sh $1/audio/Tests/19839/19839-01.mp3
 
 python3 clean.py
