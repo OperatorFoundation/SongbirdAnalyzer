bcb0d07f19353fc3e96a62ecd235e31bc0c0a582
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2025-02-03 15:48:51 -0600

    Added python-based trainer

---
+++ predict.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv("results/training.csv")
+X = df.drop(columns=["speaker"])
+y = df["speaker"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, predictions))
