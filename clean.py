5194f069d57433c5109752bb87d39cf028f6a59e
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2025-04-03 14:54:29 -0500

    Don't delete short files, instead we now skip them if mfcc extraction fails

--- clean.py
+++ clean.py
@@ -5,7 +5,7 @@ import glob
 path = sys.argv[1]
 
 # Removes any audio files that are not the standardized size
-for user in os.listdir(path):
-    for wav in glob.glob(f'{path}/{user}/*.wav'):
-        if os.path.getsize(wav) < 1323044:
-            os.remove(wav)
+#for user in os.listdir(path):
+#    for wav in glob.glob(f'{path}/{user}/*.wav'):
+#        if os.path.getsize(wav) < 1323044:
+#            os.remove(wav)
