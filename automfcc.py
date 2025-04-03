aeb4772dd94b2bcd4d60adaa68ea7c032b49969d
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2025-04-03 14:53:55 -0500

    Ignore failures to extract mfccs, which occur on short wav files

--- automfcc.py
+++ automfcc.py
@@ -23,45 +23,49 @@ for user in users:
         audio_file = wav
         signal, sr = librosa.load(audio_file)
 
-        mfccs_seq = librosa.feature.mfcc(y=signal, n_mfcc=13, sr=sr)
-        mfccs = list(mfccs_seq[0])
+        try:
+            mfccs_seq = librosa.feature.mfcc(y=signal, n_mfcc=13, sr=sr)
+            mfccs = list(mfccs_seq[0])
 
-        delta_mfccs_seq = librosa.feature.delta(mfccs_seq)
-        delta_mfccs = list(delta_mfccs_seq[0])
+            delta_mfccs_seq = librosa.feature.delta(mfccs_seq)
+            delta_mfccs = list(delta_mfccs_seq[0])
 
-        delta2_mfccs_seq = librosa.feature.delta(mfccs_seq, order=2)
-        delta2_mfccs = list(delta2_mfccs_seq[0])
+            delta2_mfccs_seq = librosa.feature.delta(mfccs_seq, order=2)
+            delta2_mfccs = list(delta2_mfccs_seq[0])
 
-        plt.figure(figsize=(25, 10))
-        librosa.display.specshow(mfccs_seq,
-                                 x_axis="time",
-                                 sr=sr)
-        plt.colorbar(format="%+2.f")
-        plt.savefig("images/"+Path(audio_file).stem+"1.png")
-        plt.close()
+            plt.figure(figsize=(25, 10))
+            librosa.display.specshow(mfccs_seq,
+                                     x_axis="time",
+                                     sr=sr)
+            plt.colorbar(format="%+2.f")
+            plt.savefig("images/"+Path(audio_file).stem+"1.png")
+            plt.close()
 
-        plt.figure(figsize=(25, 10))
-        librosa.display.specshow(delta_mfccs_seq,
-                                 x_axis="time",
-                                 sr=sr)
-        plt.colorbar(format="%+2.f")
-        plt.savefig("images/"+Path(audio_file).stem+"2.png")
-        plt.close()
+            plt.figure(figsize=(25, 10))
+            librosa.display.specshow(delta_mfccs_seq,
+                                     x_axis="time",
+                                     sr=sr)
+            plt.colorbar(format="%+2.f")
+            plt.savefig("images/"+Path(audio_file).stem+"2.png")
+            plt.close()
 
-        plt.figure(figsize=(25, 10))
-        librosa.display.specshow(delta2_mfccs_seq,
-                                 x_axis="time",
-                                 sr=sr)
-        plt.colorbar(format="%+2.f")
-        plt.savefig("images/"+Path(audio_file).stem+"3.png")
-        plt.close()
+            plt.figure(figsize=(25, 10))
+            librosa.display.specshow(delta2_mfccs_seq,
+                                     x_axis="time",
+                                     sr=sr)
+            plt.colorbar(format="%+2.f")
+            plt.savefig("images/"+Path(audio_file).stem+"3.png")
+            plt.close()
 
-        if first_row:
-            header = ["speaker"] + [f"MFCC_{i + 1}" for i in range(len(mfccs))] + [f"Delta_{i + 1}" for i in range(len(delta_mfccs))] + [f"Delta2_{i + 1}" for i in range(len(delta2_mfccs))]
-            csvout.writerow(header)
-            first_row = False
+            if first_row:
+                header = ["speaker"] + [f"MFCC_{i + 1}" for i in range(len(mfccs))] + [f"Delta_{i + 1}" for i in range(len(delta_mfccs))] + [f"Delta2_{i + 1}" for i in range(len(delta2_mfccs))]
+                csvout.writerow(header)
+                first_row = False
 
-        csvout.writerow([user] + mfccs + delta_mfccs + delta2_mfccs)
+            csvout.writerow([user] + mfccs + delta_mfccs + delta2_mfccs)
+        except Exception as e:
+            print(f'Error processing {wav} ({e})')
+            continue
 
 
 resultsFile.close()
